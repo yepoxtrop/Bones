988b478ee94f6d123e7b2f74479a72ef1717af60
neo <lsarmiento@aciel.co>
2026-06-30 16:41:21 -0500

    Add last updates

---
+++ src/config/environment_variables.py
@@ -0,0 +1,12 @@
+import os;
+
+FIRST_NAME=os.getenv("FIRST_NAME");
+SECOND_NAME=os.getenv("SECOND_NAME");
+LAST_NAME=os.getenv("LAST_NAME");
+NICK_NAME=os.getenv("NICK_NAME");
+AREA=os.getenv("AREA");
+PROFESSION=os.getenv("PROFESSION");
+HOST_NAME=os.getenv("HOST_NAME");
+OPERATIVE_SYSTEM=os.getenv("OPERATIVE_SYSTEM");
+ACTUAL_USER=os.getenv("ACTUAL_USER");
+PUBLIC_IP=os.getenv("PUBLIC_IP");
